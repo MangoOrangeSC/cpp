bcfb87a6c27c534eca84d3f2029fceda685a7442
casicApollo <373915439@qq.com>
2022-08-10 11:41:03 +0800

    pointer error

---
+++ 2.C++Primer/1.C++基础入门/代码/pointer/main.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+class A
+{
+private:
+    /* data */
+    int a;
+public:
+    A(int aa):a(aa){};
+    ~A();
+};
+
+class B
+{
+private:
+    /* data */
+    int b;
+public:
+    B(int bb):b(bb){};
+    ~B();
+};
+
+
+
+
+
+int main()
+{
+/*
+    double m = 0.5;
+    int *p = nullptr;
+    p = &m; //error
+*/
+
+//error
+/*
+    A a_(5);
+    B *b_ = &a_;
+*/
+}
